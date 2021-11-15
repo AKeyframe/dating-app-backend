1eb88ef1550d8a9ce182093cbaae84fb4cd4e820
AKeyframe <kagebro@gmail.com>
2021-11-14 22:08:14 -0600

    Auth and basic server

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+const salt = 12;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true, unique: true},
+    first: String,
+    last: String,
+}, {
+  timestamps: true
+});
+
+userSchema.methods.comparePassword = function(tryPass, cb){
+   
+    bcrypt.compareSync(tryPass, this.passsword);
+}
+
+userSchema.set('toJSON', {
+  transform: function (doc, ret) {
+    // remove the password property when serializing doc to JSON
+    delete ret.password;
+    return ret;
+  }
+});
+
+//Before the user is saved
+//Mongoose Middleware .pre
+userSchema.pre('save', function(next) {
+    const user = this;
+    //Check to see if the password has been changed
+    //if it hasn't return, otherwise hash and salt
+    if(!user.isModified('password')) return next();
+    bcrypt.hash(user.password, salt, (error, hash) => {
+        if(error) return next(error);
+        //replace user password with the hash
+        user.password = hash;
+        next();
+    });
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const {PORT=4000, MONGODB_URL} = process.env;
+
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+
+//Connect to the database
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
+mongoose.connection
+  .on("open", () => console.log("You are connected to mongoose"))
+  .on("close", () => console.log("You are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
+
+const User = require('./models/User');
+const userController = require('./controllers/users');
+
+/////////////////////////////////////////////////////////////////////////////
+//                          Middleware
+/////////////////////////////////////////////////////////////////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+app.use('/', userController);
+
+
+/////////////////////////////////////////////////////////////////////////////
+//                          Default Routes
+/////////////////////////////////////////////////////////////////////////////
+app.get("/", (req, res) => {
+    res.send("hello world");
+  });
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
